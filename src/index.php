3bccfcd30b9f6d35a2bd249c1854562b1e560240
MrEkkito <rg21057@ues.edu.sv>
2025-09-04 12:55:45 -0600

    Cambios finales

--- src/index.php
+++ src/index.php
@@ -1,38 +1,25 @@
-<?php 
-
-$filtrado = "";
-
-$personajes = [
-    [
-        "nombre" -> "lord farquaad",
-        "imagen" -> "src/images/img1"
-    ],
-    [
-
-    ],
-    
-]
-?>
-
+<!-- Ejercicio 2-->
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Personajes de Shrek</title>
+    <title>Document</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <h1>Personajes de shrek</h1>
-    <p>Filtrar por categoria<p>
-    <a href="#home" name="villano">Personaje principal</a>
-    <a href="#home" name="villano">Amigos de shrek</a>
-    <a href="#home" name="villano">Villanos </a>
-    <a href="#home" name="villano">Personajes secundarios</a>
+    <p class="titulo">Calculadora de temperaturas</p>
 
-    <div class="card">
-<?php 
-    if ($filtrado = )
-        foreach ()
+    <form> 
+    <label>Ingrese la temperatura</label>
+    <input type="number" name="valor" required>
+    <br>
+    <label> Ingrese a que desea convertir </label>
+    <input type="select" required>
+    <comm>
+
+<?php
+?>
+</form>
 </body>
 </html>
---
+++ src/index1.php
@@ -0,0 +1,61 @@
+<!--Ejercicio 1 -->
+<?php 
+
+$filtrado = "";
+
+$personajes = [
+    [
+        "nombre" => "lord farquaad",
+        "imagen" => "src/images/img1"
+    ],
+    [
+        "nombre" => "Hermanastra fea",
+        "imagen" => "src/images/img2.jpg"
+    ],
+    [
+        "nombre" = "Shrek",
+        "imagen" = "src/images/img3.jpg"
+    ],
+    [
+        "nombre" = "Burro",
+        "imagen" = "src/images/img4.jpg"
+    ],
+    [
+        "nombre" = "jenji",
+        "imagen" = "src/image/img4.jpg"
+    ]
+];
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Personajes de Shrek</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <h1>Personajes de shrek</h1>
+    <p>Filtrar por categoria<p>
+    <a href="#home" name="villano">Personaje principal</a>
+    <a href="#home" name="villano">Amigos de shrek</a>
+    <a href="#home" name="villano">Villanos </a>
+    <a href="#home" name="villano">Personajes secundarios</a>
+
+    <div class="card">
+        <!--Aqui vamos a imprimir filtrando los personajes -->
+<?php 
+    if ($filtrado = )
+        foreach ($personajes as $p){
+        echo "<div class='personajes'>";
+        echo "<p class='nombrePersonaje'>".$p['nombre'];
+        echo
+    }
+?>
+<img src="src/images/img1.jpg" alt="Imagen del personaje">
+
+</div>    
+</body>
+<!-- Aqui quiero que me escribas en comentarios los comandos para añadir archivos al commit usando git bash, tambien ponerle nombre y enviar al origen
+</html>
